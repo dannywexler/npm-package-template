a5103e4f688772071b3e58b24b86b9d904ccbe2f
Danny Wexler <dannywexler@gmail.com>
2025-10-19 21:30:17 -0700

    Updated to use tsdown, lefthook and biome v2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World")
+export function greet(name: string) {
+    return `Hello ${name}`
+}
+
+if (import.meta.vitest) {
+    const { it, expect } = import.meta.vitest
+
+    it("Can test right in source code", () => {
+        expect(greet("Name")).toEqual("Hello Name")
+    })
+}
